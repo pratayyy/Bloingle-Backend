15157b4994121abdd22c7c387fee2391d70c7059
pratayyy <pratayroy_official@yahoo.com>
2024-03-22 18:01:58 +0530

    basic blog model added

---
+++ models/blogModel.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const blogSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'A blog must have title'],
+      maxlength: [40, 'A blog name must have less or equal then 40 characters'],
+      minlength: [10, 'A blog name must have more or equal then 10 characters'],
+    },
+    slug: String,
+    banner: {
+      type: String,
+      // required: true,
+    },
+    description: {
+      type: String,
+      maxlength: [
+        200,
+        'A blog must have description less then or equal to 200 characters',
+      ],
+      // required: true
+    },
+    content: {
+      type: [],
+      // required: true
+    },
+    tags: {
+      type: [String],
+      // required: true
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    activity: {
+      totalLikes: {
+        type: Number,
+        default: 0,
+      },
+      totalComments: {
+        type: Number,
+        default: 0,
+      },
+      totalReads: {
+        type: Number,
+        default: 0,
+      },
+      totalParentComments: {
+        type: Number,
+        default: 0,
+      },
+    },
+    comments: {
+      type: [mongoose.Schema.ObjectId],
+      ref: 'Comment',
+    },
+    draft: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'publishedAt',
+    },
+  },
+);
+
+blogSchema.pre('save', function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
--- models/userModel.js
+++ models/userModel.js
@@ -29,89 +29,96 @@ const profileImagesCollectionsList = [
   'fun-emoji',
 ];
 
-const userSchema = mongoose.Schema({
-  personalInfo: {
-    name: {
-      type: String,
-      required: [true, 'Please provide your name!'],
-      minlength: [3, 'Name must be atleast 3 characters'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please provide your email!'],
-      unique: true,
-      lowercase: true,
-      validate: [validator.isEmail, 'Please provide a valid email'],
-    },
-    password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minlength: 8,
-      select: false,
-    },
-    username: {
-      type: String,
-      minlength: [3, 'Username must be 3 letters long'],
-      unique: true,
-    },
-    bio: {
-      type: String,
-      maxlength: [200, 'Bio should not be more than 200'],
-      default: '',
-    },
-    photo: {
-      type: String,
-      default: () => {
-        return `https://api.dicebear.com/6.x/${profileImagesNameList[Math.floor(Math.random() * profileImagesNameList.length)]}/svg?seed=${profileImagesNameList[Math.floor(Math.random() * profileImagesNameList.length)]}`;
+const userSchema = mongoose.Schema(
+  {
+    personalInfo: {
+      name: {
+        type: String,
+        required: [true, 'Please provide your name!'],
+        minlength: [3, 'Name must be atleast 3 characters'],
+      },
+      email: {
+        type: String,
+        required: [true, 'Please provide your email!'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+      },
+      password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        select: false,
+      },
+      username: {
+        type: String,
+        minlength: [3, 'Username must be 3 letters long'],
+        unique: true,
+      },
+      bio: {
+        type: String,
+        maxlength: [200, 'Bio should not be more than 200'],
+        default: '',
+      },
+      photo: {
+        type: String,
+        default: () => {
+          return `https://api.dicebear.com/6.x/${profileImagesNameList[Math.floor(Math.random() * profileImagesNameList.length)]}/svg?seed=${profileImagesNameList[Math.floor(Math.random() * profileImagesNameList.length)]}`;
+        },
       },
     },
+    socialLinks: {
+      youtube: {
+        type: String,
+        default: '',
+      },
+      instagram: {
+        type: String,
+        default: '',
+      },
+      facebook: {
+        type: String,
+        default: '',
+      },
+      twitter: {
+        type: String,
+        default: '',
+      },
+      github: {
+        type: String,
+        default: '',
+      },
+      website: {
+        type: String,
+        default: '',
+      },
+    },
+    accountInfo: {
+      totalPosts: {
+        type: Number,
+        default: 0,
+      },
+      totalReads: {
+        type: Number,
+        default: 0,
+      },
+    },
+    google_auth: {
+      type: Boolean,
+      default: false,
+    },
+    blogs: {
+      type: [mongoose.Schema.ObjectId],
+      ref: 'Blog',
+      default: [],
+    },
   },
-  socialLinks: {
-    youtube: {
-      type: String,
-      default: '',
-    },
-    instagram: {
-      type: String,
-      default: '',
-    },
-    facebook: {
-      type: String,
-      default: '',
-    },
-    twitter: {
-      type: String,
-      default: '',
-    },
-    github: {
-      type: String,
-      default: '',
-    },
-    website: {
-      type: String,
-      default: '',
+  {
+    timestamps: {
+      createdAt: 'joinedAt',
     },
   },
-  accountInfo: {
-    totalPosts: {
-      type: Number,
-      default: 0,
-    },
-    totalReads: {
-      type: Number,
-      default: 0,
-    },
-  },
-  google_auth: {
-    type: Boolean,
-    default: false,
-  },
-  blogs: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Blog',
-    default: [],
-  },
-});
+);
 
 const User = mongoose.model('User', userSchema);
 
