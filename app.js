2fe5242bc54dc5e60932a5263234a1aa92f3e1d5
pratayyy <pratayroy_official@yahoo.com>
2024-03-22 17:26:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+module.exports = app;
