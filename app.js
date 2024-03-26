a6b06854c19bd6b132da7e81fcb94bbeb937a1c9
pratayyy <pratayroy_official@yahoo.com>
2024-03-26 18:58:47 +0530

    complete error handling utilites added

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoute');
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -11,5 +19,11 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
