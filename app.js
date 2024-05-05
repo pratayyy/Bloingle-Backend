ce27c042d657753b58767db57637f8426137c379
pratayyy <pratayroy_official@yahoo.com>
2024-05-05 16:00:17 +0530

    max access limit added

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const limiter = rateLimiter({
-  max: 100,
+  max: 10000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
   validate: { xForwardedForHeader: false },
